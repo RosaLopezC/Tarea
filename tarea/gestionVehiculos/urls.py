bea2c308b9cb0b459b4d9db35ff4a9700920f28c
RosaLopezC <rosalopezxd@gmail.com>
2024-09-04 15:31:12 -0500

    Initial commit

--- tarea/gestionVehiculos/urls.py
+++ tarea/gestionVehiculos/urls.py
@@ -3,15 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('vehiculo/<int:vehiculo_id>/registrar_entrada/', views.registrar_entrada, name='registrar_entrada'),
     path('registro/<int:registro_id>/registrar_salida/', views.registrar_salida, name='registrar_salida'),
     path('registrar_propietario/', views.registrar_propietario, name='registrar_propietario'),
     path('propietario/<int:propietario_id>/registrar_vehiculo/', views.registrar_vehiculo, name='registrar_vehiculo'),
     path('propietario/<int:propietario_id>/vehiculos/', views.listar_vehiculos, name='listar_vehiculos'),
     path('vehiculo/<int:vehiculo_id>/editar/', views.editar_vehiculo, name='editar_vehiculo'),
     path('vehiculo/<int:vehiculo_id>/eliminar/', views.eliminar_vehiculo, name='eliminar_vehiculo'),
+    path('vehiculo/<int:vehiculo_id>/registrar_entrada/', views.registrar_entrada, name='registrar_entrada'),
     path('registros/', views.listar_registros, name='listar_registros'),
     path('registros/historial/<int:vehiculo_id>/', views.historial_registro, name='historial_registro'),
-
-
 ]
